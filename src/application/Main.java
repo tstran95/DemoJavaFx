adfe7d7e5c87d0b23f4389169e7246eb0c95bfed
tstram95 <ttson3@ttson3.ldap.cmcglobal.com.vn>
2022-04-26 15:47:50 +0700

    add Main2

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,31 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent> {
+	Button button1;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			primaryStage.setTitle("Hello World!!!!");
 
-//			BorderPane root = new BorderPane();
-//			Parent parent = new Button("Click Me!!!!");
-			Button button = new Button();
-			button.setText("Say Hello World");
+			button1 = new Button();
+			button1.setText("Say Hello World!!!");
+//			Button button2 = new Button();
+//			button2.setText("Save The World!!!");
+			
 			StackPane layout = new StackPane();
-			layout.getChildren().add(button);
+			layout.getChildren().addAll(button1);
+			button1.setOnAction(this);
+			button1.setId("button1");
 			Scene scene = new Scene(layout,300,250);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -32,5 +37,13 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		if (event.getSource().equals(button1)) {
+			System.out.println("Hello World!!!!");			
+		}
+		
 	}
 }
